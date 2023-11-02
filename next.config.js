301f34cb49cfa33d12b886b315329ca7817713a5
Kurucz Laci <lacikurucz09@gmail.com>
2023-11-02 20:23:38 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
   env: {
     COCKTAILS_URL: "https://www.thecocktaildb.com",
     // HOST: "http://localhost:3000",
-    HOST: "https://ketonbever0.github.io/cocktails/"
+    HOST: "https://ketonbever0.github.io/cocktails"
   },
 };
 
