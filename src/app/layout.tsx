58008ab0e1adff2b9d9ecc1363c9a3f5fb912431
Kurucz Laci <lacikurucz09@gmail.com>
2023-11-04 01:10:12 +0100

    Icon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <link rel="icon" href="/icon.svg" sizes="any" />
       <body className={inter.className} data-theme="synthwave">
         <Providers>
           {/* <NavBar /> */}
