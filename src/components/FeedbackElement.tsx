5a942108f2854564d42bdabbe0854fd973e6ebf6
Kurucz Laci <lacikurucz09@gmail.com>
2023-12-26 15:00:21 +0100

    feedback formatted date

--- src/components/FeedbackElement.tsx
+++ src/components/FeedbackElement.tsx
@@ -1,11 +1,12 @@
 import { FeedbackType } from "@/types/FeedbackTypes";
+import dateFormat from "dateformat";
 
 export function FeedbackElement(feedback: FeedbackType) {
 
 
     return (
         <tr>
-            <th>{feedback.createdAt}</th>
+            <th>{dateFormat(feedback.createdAt, "dddd H:MM, mmmm dS, yyyy")}</th>
             <td>{feedback.title}</td>
             <td>{feedback.details}</td>
         </tr>
